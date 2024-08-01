302f77fd92c548851cf07c21fff37c517d4d927c
Juliano Ambrósio <gestaojuliano99@gmail.com>
2024-07-31 23:44:11 -0300

    iniciando funções

--- resources/hotel.py
+++ resources/hotel.py
@@ -27,3 +27,21 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+    
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel not found.'}, 404 #not found
+
+    def post(self, hotel_id):
+        pass
+
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
