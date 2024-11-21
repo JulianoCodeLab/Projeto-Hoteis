b9551db2cc9b93acd9eaec04fb8c4d158c89e5e7
Juliano Augusto Da Silva Ambrosio <gestaojuliano99@gmail.com>
2024-11-21 13:28:49 -0300

    reestruturando o CRUDE

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hoteis, Hotel
+from resources.hotel import Hoteis
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.bd' #criando banco de dados direto da raiz
-app.config['SQLALCHEMY_TRCK_MODIFICATIONS'] = False
-
-#Antes da primeira requisição ele cria o banco.
-@app.before_request
-def cria_banco():
-    banco.create_all()
-
-
 api.add_resource(Hoteis, '/hoteis')
 
-api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
-
-
 if __name__ == '__main__':
-
-    from  sql_alchemy import banco
-    banco.init_app(app)
-
     app.run(debug=True)
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,87 +1,42 @@
-from flask_restful import Resource, reqparse
-from models.hotel   import HotelModel
+from flask_restful import Resource
 
-
-# Lista inicial de hotéis para servir como base de dados.
 hoteis = [
     {
         'hotel_id': 'alpha',
-        'nome': 'Alpha Hotel',
-        'estrelas': 4.3,
+        'nome': 'Alpha Hoteis',
+        'estrelas': '4.3',
         'diaria': 420.34,
         'cidade': 'Rio de Janeiro'
     },
     {
-        'hotel_id': 'bravo',
-        'nome': 'Bravo Hotel',
-        'estrelas': 4.4,
-        'diaria': 380.90,
-        'cidade': 'Santa Catarina'
+            'hotel_id': 'bravo',
+            'nome': 'Bravo Hoteis',
+            'estrelas': '4.4',
+            'diaria': 380.90,
+            'cidade': 'Santa Catarina'
     },
     {
-        'hotel_id': 'charlie',
-        'nome': 'Charlie Hotel',
-        'estrelas': 3.9,
-        'diaria': 320.20,
-        'cidade': 'Santa Catarina'
-    }
+            'hotel_id': 'charlie',
+            'nome': 'Charlie Hoteis',
+            'estrelas': '3.9',
+            'diaria':320.20,
+            'cidade': 'Santa Catarina'
+    },
 ]
 
-# Classe que representa os hotéis
-
-
-#-----------------------------------------------------------------------------------------
-# Classe Hoteis para lidar com a coleção de hotéis.
 class Hoteis(Resource):
-
-    # Metodo GET para retornar todos os hotéis.
     def get(self):
         return {'hoteis': hoteis}
-    
-#-----------------------------------------------------------------------------------------
 
-# Classe Hotel para lidar com operações relacionadas a um único hotel.
 class Hotel(Resource):
+    def get(self):
+        pass
 
-    # Definindo construtor | atributos da classe Hotel
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
+    def post(self):
+        pass
 
-    # Metodo GET para retornar um hotel específico pelo ID. Se o hotel não for encontrado, retorna um erro 404.
-    def get(self, hotel_id):
-        hotel = HotelModel.find_hotel(hotel_id)
-        if hotel:
-            return hotel.json() # retorna a resposta Json do hotel
-        return {'message': 'Hotel not found.'}, 404  # not found
+    def put(self):
+        pass
 
-    # Metodo POST para adicionar um novo hotel.
-    def post(self, hotel_id):
-        if HotelModel.find_hotel(hotel_id):
-            return {'message' : f'Hotel id "{hotel_id}" already exists.'.format(hotel_id)}, 400 #requisição errada
-
-        dados = Hotel.argumentos.parse_args()  # Correção: Usado "argumentos" em vez de "atributos"
-        hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
-        return hotel.json(), 201 #retorna o hotel criado
-
-    # Metodo PUT para atualizar um hotel existente, ou criar um se ele não existir.
-    def put(self, hotel_id):
-        dados = Hotel.argumentos.parse_args()  # Correção: Usado "argumentos" em vez de "atributos"
-        novo_hotel = HotelModel(hotel_id, **dados)
-        hotel = HotelModel.find_hotel(hotel_id)
-
-        if hotel:
-            hotel.update(novo_hotel.json())
-            return hotel, 200  # ok
-        else:
-            novo_hotel.save_hotel()
-            return novo_hotel, 201  # created
-
-    # Metodo DELETE para remover um hotel pelo ID.
-    def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deleted'}
+    def delete(self):
+        pass
