14c60e450f9cfce768348b2d7b1acd8a477247fb
Juliano Augusto Da Silva Ambrosio <gestaojuliano99@gmail.com>
2024-11-21 15:31:40 -0300

    metodos crude criados

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource
+from click import argument
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -23,20 +24,57 @@ hoteis = [
             'cidade': 'Santa Catarina'
     },
 ]
-
+#-----------------------
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
 
+#------------------------
 class Hotel(Resource):
-    def get(self):
-        pass
 
-    def post(self):
-        pass
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
 
-    def put(self):
-        pass
+    def find_hotel(hotel_id): #funcao que procura hotel
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
 
-    def delete(self):
-        pass
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found.'},404 #not found
+
+
+    def post(self, hotel_id):
+        dados = Hotel.argumentos.parse_args()
+
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
+
+    def put(self, hotel_id):
+
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)    #funcao para atualizar os dados
+            return novo_hotel, 200      #200 -> ok
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201          #created -> criado
+
+    def delete(self, hotel_id):
+        global hoteis
+
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted.'}
